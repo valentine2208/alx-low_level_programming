471bcf3ffcc94c9ec7e75e55d4c0407dda828d97
valentine2208 <valentinefrncs@gmail.com>
2022-09-16 06:28:11 -0700

    Print with printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main- Serves as the entry point for the program
+ *
+ * Return: By defult, returs value to zero otherwise non-zero
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- Serves as the entry point for the program
+ *
+ * return: By defualt returns to zero otherwise non-zero
+ */
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}
